731f926d3fe5881e5f421841174f0977a77c493a
RobinHanxy <717740827@qq.com>
2022-05-23 11:00:11 +0800

    Fix bugs.

--- extract_border_from_image.py
+++ extract_border_from_image.py
@@ -125,8 +125,7 @@ def mix_images(origin_img, edited_img, segmentors):
     if isinstance(segmentors, dict):
         img = origin_img.copy()
         hair_segmentor = segmentors['hair_segmentor']
-        hair_mask = hair_segmentor.segment(img)
-        hair_mask *= 255
+        hair_mask = hair_segmentor.segment(img) * 255
     else:
         hair_mask = get_hair_mask(img_path=origin_img, net=segmentors, include_hat=True, include_ear=True)
 
@@ -167,9 +166,9 @@ def extract_head_border_from_image(origin_img, encode_net, model, mapper, alpha,
         # amazing
         img = edited_img.copy()
         face_segmentor = segmentors['face_segmentor']
-        # hair_segmentor = segmentors['hair_segmentor']
+        hair_segmentor = segmentors['hair_segmentor']
         face_mask = face_segmentor.segment(img) * 255
-        # hair_mask = hair_segmentor.segment(img)
+        hair_mask = hair_segmentor.segment(img) * 255
         # mask = np.bitwise_or(face_mask, hair_mask).astype('uint8')
         mask = face_mask.astype('uint8')
     else:
